5a0043083b0ac619ba1ea5da8888379e4368b9ee
Harsh <harshitraj6591@gmail.com>
2025-02-22 19:19:04 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // Available Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/quiz", require("./routes/quiz"));
-app.use("/api/lecture", require("./routes/lectures"));
-app.use("/api/dashboard", require("./routes/Dashboard"));
+app.use("api/auth", require("./routes/auth"));
+app.use("api/quiz", require("./routes/quiz"));
+app.use("api/lecture", require("./routes/lectures"));
+app.use("api/dashboard", require("./routes/Dashboard"));
 
 app.listen(port, () => {
   console.log(` Backend started at http://localhost:${port}`);
