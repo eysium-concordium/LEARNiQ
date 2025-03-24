f4c26ca649d3b49a07899a708680976800c4d770
Krishna240517 <codexgame7@gmail.com>
2025-03-24 15:59:37 +0530

    added the async await syntax in connectToMongo for db connection

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const mongoURI = process.env.mongoURI;
 
-const connectToMongo = () => {
-  mongoose.connect(mongoURI);
+const connectToMongo = async() => {
+  await mongoose.connect(mongoURI);
 };
 module.exports = connectToMongo;
